76e3b83e2a7665ffc9bff428a0fa4e0dc7d4ebde
nelsonsequiera <nelsonsequiera@gmail.com>
2017-06-09 22:15:52 +0530

    add postgres

--- vasooli/settings.py
+++ vasooli/settings.py
@@ -102,13 +102,23 @@ WSGI_APPLICATION = 'vasooli.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DATABASE_NAME', default='vasooli'),
+        'HOST': env('DATABASE_HOST', default='localhost'),
+        'USER': env('DATABASE_USER', default='vasooli'),
+        'PASSWORD': env('DATABASE_PASSWORD', default='vasooli'),
+        'PORT': env('DATABASE_PORT', default='5432'),
+    },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
