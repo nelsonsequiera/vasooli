9397def66cdedd4a679901efb968fe0aff10e530
nelsonsequiera <nelsonsequiera@gmail.com>
2017-06-10 13:28:55 +0530

    statuc

--- vasooli/settings.py
+++ vasooli/settings.py
@@ -163,10 +163,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'assets'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
