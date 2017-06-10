b035d383ef722ccd3f2bcb75598fff2c8419701b
Harsha Ashok <harshaashok93@gmail.com>
2017-06-10 10:22:32 +0530

    templates created.

--- authorization/views.py
+++ authorization/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class LoginView(TemplateView):
+    template_name = 'authorization/login.html'
+
+
+class RegisterView(TemplateView):
+    template_name = 'authorization/register.html'
